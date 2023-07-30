4831783d0631e82ae76cb875f6e4b170d3b7b6b9
Dale <dludwins@outlook.com>
2023-07-30 16:49:23 -0400

    added config file

---
+++ helpers/config.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 30 16:31:59 2023
+
+@author: dale
+"""
+
+
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Settings:
+    
+    SUPA_PASSWORD = os.getenv('SUPA_URL')
+    SUPA_ID = os.getenv('SUPA_URL')
+    SUPA_PORT = os.getenv('SUPA_PORT')
+    SUPA_URL = f'{SUPA_PASSWORD}@db.{SUPA_ID}.supabase.co:{SUPA_PORT}'
+    DB_URL: str = f'postgresql://postgres:{SUPA_URL}/postgres'
+    
+    
+settings = Settings()
